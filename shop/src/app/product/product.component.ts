b5eb96c8d48719e6710601f89d0286118e47a741
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-03-06 16:10:43 +0300

    product component update

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  title = "Product List";
+  products : any[] = [
+    {
+      id:1,
+      name:"Laptop",
+      price: 780,
+      categoryId:1,
+      description:"HP"
+    },
+    { 
+      id:2,
+      name:"Phone",
+      price:3500,
+      categoryId:2,
+      description:"IPhone"
+    }
+  ];
+  constructor(){  
+      
+  }
+  ngOnInit() {
+
+  }
 
 }
