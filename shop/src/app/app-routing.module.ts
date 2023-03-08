d04c789cc26fb7be786fa5be895d03f0113cecd8
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-03-08 22:21:10 +0300

    design update

--- shop/src/app/app-routing.module.ts
+++ shop/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
+
   {
     path:'products', component: ProductComponent
   },
   {
     path:"products/category/:categoryId", component: ProductComponent
+  },
+  {
+    path:'/add-product-1', component: ProductComponent
+  },
+  {
+    path:'/add-product-2', component: ProductComponent
   },
   {
     path:"", redirectTo: "products", pathMatch:"full"
